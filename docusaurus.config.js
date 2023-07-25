dc9b5744f6f6ea2e4bbfa3ea78740070926d9a89
曾小皮-ZengXiaoPi <1768506869@qq.com>
2023-07-25 10:49:10 +0800

    Init

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Kabala',
+  tagline: '卡巴拉',
+  url: 'https://kabala.zeroasso.top',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ZengXiaoPi', // Usually your GitHub org/user name.
+  projectName: 'Kabala-Wiki', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+  
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Kabala',
+        logo: {
+          alt: 'Kabala',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'main',
+            position: 'left',
+            label: '主页',
+          },
+          {to: '/blog', label: '博客', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '页面',
+            items: [
+              {
+                label: '主页',
+                to: '/docs/main',
+              },
+            ],
+          },
+          {
+            items: [
+              {
+                label: '2item',
+                to: '/docs/main',
+              },
+            ],
+          },
+          {
+            title: '别的东西',
+            items: [
+              {
+                label: '博客',
+                to: '/blog',
+              },
+            ],
+          },
+        ],
+        copyright: `版权所有 © 2023 Kabala . 本网站使用 Docusaurus 构建`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
